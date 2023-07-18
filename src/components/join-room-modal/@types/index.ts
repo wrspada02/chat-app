19b31eb1f200945507da48706616c38e0118342a
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2023-07-18 20:43:33 -0300

    style: add responsive mode to the create room modal; feat: add form validation with zod

--- src/components/join-room-modal/@types/index.ts
+++ src/components/join-room-modal/@types/index.ts
@@ -1,6 +1,20 @@
+import { z } from "zod";
 import { TypeHandleModalRoom } from "../../../screens/room/@types";
 
 export interface JoinRoomModalProps {
   isJoinRoom: TypeHandleModalRoom;
   handleClose: () => void;
 }
+
+export const roomSchema = z.object({
+  is_private: z.boolean()
+  .describe('Private room is not able to access without a password'),
+  room_id: z.string()
+  .describe('The name of the room')
+  .min(5),
+  room_password: z.string()
+  .describe('The password for a private room')
+  .min(8),
+}).required();
+
+export type Room = z.infer<typeof roomSchema>;
--- src/components/join-room-modal/index.tsx
+++ src/components/join-room-modal/index.tsx
@@ -1,16 +1,27 @@
-import { useEffect, useMemo } from "react";
-import { JoinRoomModalProps } from "./@types";
+import { FormEvent, useCallback, useEffect, useMemo, useState } from "react";
+import { JoinRoomModalProps, Room, roomSchema } from "./@types";
 
 export function JoinRoomModal({ isJoinRoom, handleClose }: JoinRoomModalProps) {
-  const roomTitle = useMemo(() => {
-    return ['join'].includes(isJoinRoom) ? 'Join into a Room' : 'Create a room';
-  }, [isJoinRoom]);
-
+  const [room, setRoom] = useState<Room>({
+    is_private: false,
+    room_id: '',
+    room_password: '',
+  });
   const handleCloseByEscButton = (event: KeyboardEvent) => {
     if (event.code === 'Escape') {
       handleClose();
     }
   };
+
+  const roomTitle = useMemo(() => {
+    return ['join'].includes(isJoinRoom) ? 'Join into a Room' : 'Create a room';
+  }, [isJoinRoom]);
+
+  const handleSubmitRoomForm = useCallback((event: FormEvent) => {
+    event.preventDefault();
+
+    console.log(roomSchema.parse(room));
+  }, [room]);
 
   useEffect(() => {
     document.addEventListener('keydown', handleCloseByEscButton);
@@ -23,33 +34,52 @@ export function JoinRoomModal({ isJoinRoom, handleClose }: JoinRoomModalProps) {
   return (
     <section className="absolute top-0 left-0 bg-black opacity-80
       w-[100vw] h-[100vh] flex items-center justify-center">
-      <article className="p-16 bg-[#D9D9D9] animate-pulse">
+      <form className="mobile:p-5 desktop:p-16 bg-[#D9D9D9] animate-pulse" onSubmit={handleSubmitRoomForm}>
         <header className="flex items-center justify-between h-5">
-          <h2 className="text-2xl font-semibold">{roomTitle}</h2>
+          <h2 className="text-2xl font-bold text-[#785BD7]">{roomTitle}</h2>
           <button className="text-2xl" onClick={handleClose}>&#x2715;</button>
         </header>
-        <article className="flex items-center justify-between gap-5 p-8">
+        <article className="mobile:flex mobile:flex-wrap mobile:p-2 mobile:gap-3 mobile:flex-col 
+          tablet:items-center tablet:justify-between tablet:gap-5 tablet:p-8 desktop:flex-row">
             <div className="flex gap-3">
-                <label>Is Private?</label>
-                <input type="checkbox" />
+                <label className="text-[#785BD7] font-semibold">Is Private?</label>
+                <input type="checkbox" checked={room?.is_private} 
+                  onChange={(inputValue) => {
+                    setRoom({ 
+                      ...room, 
+                      is_private: inputValue.target.checked || false,
+                    });
+                  }} />
             </div>
             <div className="flex flex-col">
-              <label>Room ID</label>
+              <label className="text-[#785BD7] font-semibold">Room ID</label>
               <input type="text" className="shadow-md outline-none p-1" 
-                placeholder="Type the ID..." />
+                placeholder="Type the ID..." value={room?.room_id}
+                  onChange={(inputValue) => {
+                    setRoom({
+                      ...room, 
+                      room_id: inputValue.target.value || '',
+                    });
+                  }} />
             </div>
             <div className="flex flex-col">
-              <label>Room Password</label>
-              <input type="text" className="shadow-md outline-none p-1" 
-                placeholder="Type the password..."/>
+              <label className="text-[#785BD7] font-semibold">Room Password</label>
+              <input type="text" className="shadow-md outline-none p-1 transition-all" 
+                placeholder="Type the password..." value={room?.room_password} 
+                  onChange={(inputValue) => {
+                    setRoom({
+                      ...room, 
+                      room_password: inputValue.target.value || '',
+                    });
+                  }} disabled={!room.is_private} />
             </div>
         </article>
         <footer>
-          <button className="bg-[#785BD7] m-auto float-right
+          <button className="mobile:w-full desktop:w-auto bg-[#785BD7] m-auto float-right
            text-white font-semibold p-4 shadow-md transition-all hover:shadow-none 
-             hover:opacity-75">Create</button>
+             hover:opacity-75" type="submit">Create</button>
         </footer>
-      </article>
+      </form>
     </section>
   );
 }
