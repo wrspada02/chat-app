0ba9e4d651e8fd906abffa2dd5040586e496e017
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2023-08-13 19:15:13 -0300

    style: adjust group message component

--- src/components/group-message/index.tsx
+++ src/components/group-message/index.tsx
@@ -2,15 +2,15 @@ import { GroupMessageProps } from "./@types";
 
 export function GroupMessage({ content, sender }: GroupMessageProps) {
     return (
-        <article className="p-3">
-            <figure className="flex flex-col">
+        <article className="p-3 flex">
+            <figure className="flex">
                 <img 
                     src={sender?.avatar_url} 
                     alt={`Avatar user ${sender?.login} image`}
-                    className="max-w-[100px] rounded-full"
+                    className="max-w-[70px] rounded-full"
                 />
                 <figcaption className="p-3 flex flex-col justify-center text-white">
-                    <p>{sender?.login}</p>
+                    <p className="font-medium">{sender?.login}</p>
                     <p>{content || ''}</p>
                 </figcaption>
             </figure>
