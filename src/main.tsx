e0919c9421a0881f1843806cb138aa51fc60c1f3
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2023-08-27 12:25:21 -0300

    fix: add basename hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { App } from './App'
 import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <HashRouter>
+  <HashRouter basename='https://wrspada02.github.io/chat-app/'>
     <App />
   </HashRouter>
 )
